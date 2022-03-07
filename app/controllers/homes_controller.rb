2b1363990198ee5dedcb95ffa659a03c622bc9c9
橋本和磨 <kazumahashimoto0421@gmail.com>
2022-03-08 00:02:30 +0900

    ページング処理と大まかなレイアウト完成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,4 +3,10 @@ class HomesController < ApplicationController
     @station = "大崎"
     @restaurants = TopRestaurant.where(station: "大崎").order(updated_at: "DESC")
   end
+
+  def station
+    @station = params[:station]
+    @restaurants = TopRestaurant.where(station: @station).order(updated_at: "DESC")
+    @top_restaurants = @restaurants.first(9).each_slice(3).to_a
+  end
 end
